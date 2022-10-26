4fa29d20b1f74909a761d32001af3a35eb91be5b
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-10-26 14:40:09 +0000

    add console.py

---
+++ console.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" This is the command line or console module """
+
+import cmd, sys
+from turtle import bye
+
+class HBNBCommand(cmd.Cmd):
+    """ this is the console class """
+    prompt = '(hbnb)'
+
+    def do_exit(self, arg):
+        """ exit the command line """
+
+        return True
+
+    def emptyline(self):
+        """Called when an empty line is entered in response to the prompt.
+
+        If this method is not overridden, it repeats the last nonempty
+        command entered.
+
+        """
+        pass
+
+    def do_EOF(self, arg):
+        """ response to EOF and exit prompt """
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
